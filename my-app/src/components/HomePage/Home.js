a76a179f970bbd6cac1db6dbd73c213e8c9ed679
Eric Choi <77644021+pidgeonforlife@users.noreply.github.com>
2022-02-05 12:34:57 -0800

    Initialized Router and setup a page

---
+++ my-app/src/components/HomePage/Home.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function HomePage() {
+    return (
+        <div>Hello!</div>
+    );
+}
+
+export default HomePage;
---
+++ my-app/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import HomePage from "../components/HomePage/Home";
+
+function Home() {
+    return (
+        <>
+        <HomePage />
+        </>
+    );
+}
+
+export default Home;
